41e107922b330fa8b4bb92fe9f2b222087754e25
chen zhuqing <chenzq1019@163.com>
2021-06-16 13:53:16 +0800

    no message

---
+++ flutter_app_uiclock/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/chenzhuqing/Desktop/chenzq/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/chenzhuqing/Desktop/GitProject/HT_FlutterDemo/flutter_app_uiclock"
+export "FLUTTER_TARGET=/Users/chenzhuqing/Desktop/GitProject/HT_FlutterDemo/flutter_app_uiclock/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/chenzhuqing/Desktop/GitProject/HT_FlutterDemo/flutter_app_uiclock/.dart_tool/package_config.json"
---
+++ flutter_jdapp_demo/ios/Pods/Target Support Files/city_pickers/city_pickers-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CityPickersPlugin.h"
+
+FOUNDATION_EXPORT double city_pickersVersionNumber;
+FOUNDATION_EXPORT const unsigned char city_pickersVersionString[];
+
