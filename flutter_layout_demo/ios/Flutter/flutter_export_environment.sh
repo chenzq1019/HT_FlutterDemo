33d33c9b0ed937e8363c8c302aaa9246a465d517
chen zhuqing <chenzq1019@163.com>
2024-03-29 11:08:59 +0800

    提交代码

--- flutter_layout_demo/ios/Flutter/flutter_export_environment.sh
+++ flutter_layout_demo/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/chenzhuqing/Desktop/chenzq/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/chenzhuqing/Desktop/GitProject/HT_FlutterDemo/flutter_layout_demo"
-export "FLUTTER_TARGET=/Users/chenzhuqing/Desktop/GitProject/HT_FlutterDemo/flutter_layout_demo/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/chenzhuqing/Desktop/GitProject/HT_FlutterDemo/flutter_layout_demo/flutter_layout_demo"
+export "FLUTTER_TARGET=/Users/chenzhuqing/Desktop/GitProject/HT_FlutterDemo/flutter_layout_demo/flutter_layout_demo/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
@@ -10,4 +10,4 @@ export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/chenzhuqing/Desktop/GitProject/HT_FlutterDemo/flutter_layout_demo/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/chenzhuqing/Desktop/GitProject/HT_FlutterDemo/flutter_layout_demo/flutter_layout_demo/.dart_tool/package_config.json"
